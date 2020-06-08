7d5f62f1395f354f84442fad0d719ee1c10c445a
Rony <ronypc@outlook.com>
2020-06-08 08:09:48 -0300

    teste

--- commands/buscar-alunos.php
+++ commands/buscar-alunos.php
@@ -1,6 +1,7 @@
 <?php
 
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '../../vendor/autoload.php';
@@ -14,5 +15,14 @@ $alunoRepository = $entityManager->getRepository(ALuno::class);
 $alunoList = $alunoRepository->findALl();
 
 foreach ($alunoList as $aluno) {
+    $telefones = $aluno
+        ->getTelefones()
+        ->map(function (Telefone $telefone) {
+            return $telefone->getNumero();
+        })
+        ->toArray();
+
     echo "ID = {$aluno->getId()}\nNome = {$aluno->getName()}\n\n";
+    echo "Telefones :" . implode(',', $telefones);
+    echo '\n\n';
 }
